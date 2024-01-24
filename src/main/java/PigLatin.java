fac7bca45600cae37b9117f5b1bda0318f28a485
Kyle Yuen <103063168+RandomHouseGuest@users.noreply.github.com>
2024-01-24 09:46:50 -0800

    PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -51,12 +51,12 @@ public class PigLatin {
         //postcondition: returns the pig latin equivalent of sWord
 	    if(sWord.length() <= 0){
 		    return sWord;
+	    } else if(sWord.substring(0, 2).equals("qu")){
+		    return sWord.substring(2) + "quay";
 	    } else if(findFirstVowel(sWord) >= 1){
 		    return sWord.substring(findFirstVowel(sWord), sWord.length()) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
 	    } else if(findFirstVowel(sWord) == 0){
 		    return sWord + "way";
-	    } else if(sWord.substring(0, 2).equals("qu")){
-		    return sWord.substring(2) + "quay";
 	    } 
 	    if(findFirstVowel(sWord) == -1) {
 		return sWord + "ay";
